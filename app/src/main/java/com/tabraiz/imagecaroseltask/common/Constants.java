da5c310be91fa6b99a54e0b3c1c987b72ee6f6d4
tabraiz_ahmad <tabraiz.ahmad95@yahoo.com>
2018-08-21 02:25:58 +0400

    code refactoring

--- app/src/main/java/com/tabraiz/imagecaroseltask/common/Constants.java
+++ app/src/main/java/com/tabraiz/imagecaroseltask/common/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 
     public static String VIDEO_STREAMING_URL =
                                   "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8";
+    public static String IMAGE_BASE_URL = "https://picsum.photos/300/200?i=";
 }
--- app/src/main/java/com/tabraiz/imagecaroseltask/common/Utils.java
+++ app/src/main/java/com/tabraiz/imagecaroseltask/common/Utils.java
@@ -3,12 +3,14 @@ package com.tabraiz.imagecaroseltask.common;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.tabraiz.imagecaroseltask.common.Constants.IMAGE_BASE_URL;
+
 public class Utils {
 
     public static List<String> getImageUriList(){
         List<String> urls = new ArrayList<>();
         for (int i=0;i<7;i++){
-            urls.add("https://picsum.photos/300/200?i="+i);
+            urls.add(IMAGE_BASE_URL + i);
         }
         return urls;
     }
