cff887acbf556efe9e9707467cd2d3e8b5cc9423
tabraiz_ahmad <tabraiz.ahmad95@yahoo.com>
2018-08-21 02:33:23 +0400

    making carousel two way

--- app/src/main/java/com/tabraiz/imagecaroseltask/caroselVideo/ui/CarouselVideoFragment.java
+++ app/src/main/java/com/tabraiz/imagecaroseltask/caroselVideo/ui/CarouselVideoFragment.java
@@ -1,6 +1,7 @@
 package com.tabraiz.imagecaroseltask.caroselVideo.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -48,23 +49,24 @@ public class CarouselVideoFragment extends BaseFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_carousel_video, container, false);
         unbinder = ButterKnife.bind(this, view);
-        if(savedInstanceState == null){
+        if(savedInstanceState == null && getActivity() != null){
             getLifecycle().addObserver(new VideoPlayerComponent(getActivity().getApplicationContext(), playerView, VIDEO_STREAMING_URL));
         }
         return view;
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         LinearLayoutManager layoutManager
                 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         carouselRecyclerView.setLayoutManager(layoutManager);
         carouselRecyclerView.setAdapter(imageCarouselAdapter);
+        carouselRecyclerView.scrollToPosition(imageCarouselAdapter.getItemCount() / 2);
     }
 }
